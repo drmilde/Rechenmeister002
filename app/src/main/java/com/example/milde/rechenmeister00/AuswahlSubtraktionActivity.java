c9cc8629755029ddd6bff3168f4aee3c09873075
drmilde <milde@hs-fulda.de>
2017-12-12 21:25:37 +0100

    AuswahlSubtraktion eingeführt und nochmal korregiert (Klassenname)

--- app/src/main/java/com/example/milde/rechenmeister00/AuswahlSubtraktionActivity.java
+++ app/src/main/java/com/example/milde/rechenmeister00/AuswahlSubtraktionActivity.java
@@ -3,7 +3,7 @@ package com.example.milde.rechenmeister00;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AuswahlSubtrationActivity extends AppCompatActivity {
+public class AuswahlSubtraktionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
